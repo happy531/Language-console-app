8fa3517dce5e0b5a932d35a6271b75d8cbfd3712
happy531 <jedrzejwesolowski177@gmail.com>
2021-11-03 12:17:11 +0100

    map through language objects and add valid language to a list

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,21 +2,40 @@ import com.google.gson.Gson;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println("Hello world");
+
 
         deserialize();
     }
 
     private static void deserialize() throws FileNotFoundException {
-        Gson gson = new Gson();
 
-        Language[] languages = gson.fromJson(new FileReader("C:\\Users\\jedrz\\IdeaProjects\\iteration\\src\\main\\java\\api.json"), Language[].class);
-        System.out.println(languages[0].getLanguage_name());
-        System.out.println(languages[0].getWords());
+        Language[] languages = new Gson().fromJson(new FileReader(
+                "C:\\Users\\jedrz\\IdeaProjects\\iteration\\src\\main\\java\\api.json"), Language[].class);
+        //System.out.println(languages[0].getLanguage_name());
+        //System.out.println(languages[0].getWords());
+
+        Scanner sc = new Scanner(System.in);
+        String inputWord = sc.nextLine();
+
+        List<String> validLanguages = new ArrayList<>();
+
+        for (Language language : languages) {
+            for (String word : language.getWords()) {
+                if (inputWord.equals(word)) {
+                    validLanguages.add(language.getLanguage_name());
+                    break;
+                }
+            }
+        }
+        if (validLanguages.size() > 0) System.out.println(validLanguages);
+        else System.out.println("There's no such word in any language");
+
 
     }
 }
