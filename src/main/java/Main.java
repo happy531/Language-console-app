4d6b72b72e496f77cb15b68f16670eaeddab7b4a
happy531 <jedrzejwesolowski177@gmail.com>
2021-11-04 23:47:28 +0100

    move serialize() and deserialize() into main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.*;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,75 +14,50 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         while (true) {
-            System.out.println("");
+            Scanner sc = new Scanner(System.in);
+            Gson gson = new GsonBuilder()
+                    .setPrettyPrinting()
+                    .create();
+
+            Type languagesListType = new TypeToken<ArrayList<Language>>() {
+            }.getType();
+            List<Language> languages = gson.fromJson(new FileReader(
+                    "C:\\Users\\jedrz\\IdeaProjects\\iteration\\src\\main\\java\\api.json"), languagesListType);
+
+            Parser parser = new Parser(languages);
+
+            System.out.println();
             System.out.println("1. Search");
             System.out.println("2. Add language and words");
             System.out.println("3. Exit");
             System.out.print("Choose action: ");
-
-            Scanner sc = new Scanner(System.in);
             String userChoice = sc.next();
 
             switch (userChoice) {
-                case "1":
-                    deserialize();
-                    break;
-                case "2":
-                    serialize();
-                    break;
-                case "3":
-                    System.out.println("exiting...");
-                    break;
-                default:
-                    System.out.println("Wrong option");
+                //deserialize
+                case "1" -> {
+                    System.out.print("Type word you're interested in: ");
+                    String inputWord = sc.next();
 
+                    System.out.println(parser.searchWord(inputWord));
+                }
+                //serialize
+                case "2" -> {
+                    Scanner sc2 = new Scanner(System.in);
+
+                    System.out.println("language name: ");
+                    String languageName = sc2.nextLine();
+
+                    System.out.println("words (separated with space): ");
+                    String words = sc2.nextLine();
+                    List<String> wordsList = Arrays.asList(words.split(" "));
+
+                    parser.addLanguage(languageName, wordsList);
+                }
+                case "3" -> System.out.println("exiting...");
+                default -> System.out.println("Wrong option");
             }
         }
-
-    }
-
-    private static void deserialize() throws FileNotFoundException {
-
-        Gson gson = new GsonBuilder()
-                .setPrettyPrinting()
-                .create();
-
-        Type languagesListType = new TypeToken<ArrayList<Language>>(){}.getType();
-        List<Language> languages = gson.fromJson(new FileReader(
-                "C:\\Users\\jedrz\\IdeaProjects\\iteration\\src\\main\\java\\api.json"), languagesListType);
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Type word you're interested in: ");
-        String inputWord = sc.next();
-
-        Parser parser = new Parser(languages);
-
-        System.out.println(parser.searchWord(inputWord));
-    }
-
-    private static void serialize() throws IOException {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("language name: ");
-        String languageName = sc.nextLine();
-
-        System.out.println("words (separated with space): ");
-        String words = sc.nextLine();
-        List<String> wordsList = Arrays.asList(words.split(" "));
-
-        Gson gson = new GsonBuilder()
-                .setPrettyPrinting()
-                .create();
-
-        Type languagesListType = new TypeToken<ArrayList<Language>>(){}.getType();
-        List<Language> languages = gson.fromJson(new FileReader(
-                "C:\\Users\\jedrz\\IdeaProjects\\iteration\\src\\main\\java\\api.json"), languagesListType);
-
-        Parser parser = new Parser(languages);
-
-        parser.addLanguage(languageName, wordsList);
-
     }
 }
+
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,17 +1,14 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
-    List<Language> languages;
+    private final List<Language> languages;
 
     public Parser(List<Language> languages) {
         this.languages = languages;
